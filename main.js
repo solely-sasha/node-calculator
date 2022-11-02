0ce5dcbda9f00b2a6536498db7c6fe7af40b9365
sasha sims <solely.sashasims@gmail.com>
2022-11-02 15:00:45 -0400

    started function for if statements

--- main.js
+++ main.js
@@ -12,3 +12,13 @@ const operations = ['add', 'subtract', 'multiply', 'divide',];
 
 const index = readlineSync.keyInSelect(operations, 'Please enter the operation to perform ');
 // console.log(operations[index])
+console.log(`operation ${operations[index]} is chosen`);
+
+function add(num1, num2){
+
+    return (parseInt(num1) + parseInt(num2))
+}
+
+if(operations[index] === 'add'){
+    console.log(`the result of ${num1} + ${num2} is`, add(num1, num2))
+}
