9cf686949c06df8804369712863ac8832eee78d5
sasha sims <solely.sashasims@gmail.com>
2022-11-02 14:57:35 -0400

    created variables

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
-const readline = require('readline-sync'); 
+var readlineSync = require('readline-sync');
 // console.log("readline is working")
+var num1 = readlineSync.question('what is your first number? ')
+
+console.log(num1)
+
+var num2 = readlineSync.question('what is your second number? ')
+
+console.log(num2)
+
+const operations = ['add', 'subtract', 'multiply', 'divide',];
+
+const index = readlineSync.keyInSelect(operations, 'Please enter the operation to perform ');
+// console.log(operations[index])
